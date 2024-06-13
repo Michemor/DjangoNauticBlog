0e0a837ea1b7d4eda9fb26ff43e72ddc59ee8973
Michemor <yatoijema@gmail.com>
2024-06-13 12:41:49 +0300

    templates tags: fetch from models and display

--- articles/views.py
+++ articles/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Article
 
 def articles(request):
-    return render(request, 'articles/articles.html')
+    # grabs all articles from the model table
+    articles = Article.objects.all().order_by('date')
+    
+    return render(request, 'articles/articles.html', {
+        'articles': articles
+    })
